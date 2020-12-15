106b83ccd84ac164c0d37efaf17cdd0c44ef3934
Liam Michaelsen <liam2000@live.dk>
2020-12-15 12:20:03 +0100

    mappe med koden til programmet

---
+++ WPF sekvensdiagrammer1/WPF sekvensdiagrammer1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using WPF_sekvensdiagrammer1.ViewModels.Commands;
+using WPF_sekvensdiagrammer1.Views.Pages;
+
+namespace WPF_sekvensdiagrammer1.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public Page PageContent { get; set; }
+
+        private ICommand setPageContentCommand { get; set; }
+        public ICommand SetPageContent { get { return setPageContentCommand; } }
+
+        public MainWindowViewModel()
+        {
+            Page home = new Home();
+            PageContent = home;
+
+            setPageContentCommand = new SetPageContent(this);
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void SwapPageContent(string name)
+        {
+            switch (name)
+            {
+                case "Home":
+                    PageContent = new Home();
+                    break;
+                case "LoginPage":
+                    PageContent = new LoginPage();
+                    break;
+                default:
+                    PageContent = new Home();
+                    break;
+            }
+            OnPropertyChanged("PageContent");
+
+        }
+
+
+        protected void OnPropertyChanged(string x)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(x));
+        }
+
+    }
+
+}
---
+++ WPF sekvensdiagrammer1/WPF sekvensdiagrammer1/Views/Pages/LoginPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_sekvensdiagrammer1.ViewModels;
+
+namespace WPF_sekvensdiagrammer1.Views.Pages
+{
+
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        { 
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string email = string.Empty;
+            string password = string.Empty;
+
+            email = txtEmail.Text;
+            password = txtPassword.Text;
+
+            string format = string.Format("Email: {0}\nPassword: {1}", email, password);
+            if (email != string.Empty && password != string.Empty)
+            {
+                MessageBox.Show(format, "User created!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Please enter user information!", "Warning!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            txtEmail.Clear();
+            txtPassword.Clear();
+
+        }
+
+    }
+
+}
